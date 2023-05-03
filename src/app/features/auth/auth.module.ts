2f152da67db41cbf424e8358e818c7913531a9e5
Nicolas Kuhn <nkuhn@teco.com.ar>
2023-05-03 16:43:44 -0300

    se añade esqueleto de paginas y rutas hacia las mismas

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,12 +1,22 @@
 // Modules
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { CommonModule } from "@angular/common";
+
 
 // Services
 
 // Components
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+]
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    LoginComponent
+  ]
 })
 export class AuthModule {}
--- src/app/features/cart/cart.module.ts
+++ src/app/features/cart/cart.module.ts
@@ -5,9 +5,18 @@ import { CommonModule } from "@angular/common";
 // Services
 
 // Components
+import { OrderComponent } from './pages/order/order.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "order", component: OrderComponent }
+]
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    OrderComponent
+  ]
 })
 export class CartModule {
 }
