a802b80352ff3f26354a3b2eab69d062bf183589
yair <yairolivait@gmail.com>
2023-05-13 11:51:50 -0300

    adding login page

--- frontend/src/app/features/auth/auth.module.ts
+++ frontend/src/app/features/auth/auth.module.ts
@@ -1,20 +1,17 @@
 // Modules
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 // Services
 
 // Components
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
-
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ]
+  imports: [CommonModule,FormsModule,MaterialModule,ReactiveFormsModule],
+  declarations: [LoginComponent, RegisterComponent],
 })
-export class AuthModule {
-}
+export class AuthModule {}
