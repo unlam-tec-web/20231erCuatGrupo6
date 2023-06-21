1b8958e180e8d621d28cdf990bb9c1e8e64ac0da
Nicolas Kuhn <nkuhn@teco.com.ar>
2023-06-21 19:42:24 -0300

    incorporo serve de imagenes en front

--- frontend/src/app/features/cart/cart.module.ts
+++ frontend/src/app/features/cart/cart.module.ts
@@ -1,19 +1,19 @@
 // Modules
 import { NgModule } from "@angular/core";
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
+import { MaterialModule } from '../../material/material.module';
 import { CommonModule } from "@angular/common";
 
 // Services
 
 // Components
-import { HttpClientModule } from "@angular/common/http";
-import { MaterialModule } from '../../material/material.module';
 import { OrderComponent } from './pages/order/order.component';
-import { RouterModule } from '@angular/router';
-import { PipeModule } from '../product/pipe/pipe.module';
+
 
 @NgModule({
-  declarations: [OrderComponent ],
-  imports: [CommonModule, HttpClientModule,MaterialModule,PipeModule,RouterModule],
+  declarations: [OrderComponent],
+  imports: [CommonModule, HttpClientModule, MaterialModule, RouterModule]
 })
 export class CartModule {
 }
