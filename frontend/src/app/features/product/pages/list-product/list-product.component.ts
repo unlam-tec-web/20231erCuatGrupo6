bbe00d7140ee7d025a4cf58a7e359f453e72ea2b
Nicolas Kuhn <nicolas.kuhn@neoris.com>
2023-06-24 00:11:08 -0300

    se parten productos x categoria

--- frontend/src/app/features/product/pages/list-product/list-product.component.ts
+++ frontend/src/app/features/product/pages/list-product/list-product.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../types/product.interface';
 import { ProductService } from '../../services/product.service';
 import { CartService } from '../../../cart/services/cart.service';
-import { Observable } from "rxjs";
+import { from, groupBy, map, mergeMap, Observable, toArray } from 'rxjs';
+
+type GroupedProduct = {
+  category: string;
+  products: Product[];
+};
 
 @Component({
   selector: 'app-list-product',
   templateUrl: './list-product.component.html',
-  styleUrls: ['./list-product.component.scss']
+  styleUrls: ['./list-product.component.scss'],
 })
 export class ListProductComponent implements OnInit {
-  public products!: Observable<Product[]>
+  public groupedProducts!: Observable<GroupedProduct[]>;
 
   constructor(
     private productService: ProductService,
@@ -18,10 +23,29 @@ export class ListProductComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.products = this.productService.findAllProducts();
+    this.groupedProducts = this.groupItemsByCategory();
   }
 
   addProduct(product: Product) {
     this.cartService.addProduct(product);
+  }
+
+  groupItemsByCategory() {
+    const products = this.productService.findAllProducts();
+
+    return products.pipe(
+      mergeMap(from),
+      groupBy((product) => product.category),
+      mergeMap((productGroup) =>
+        productGroup.pipe(
+          toArray(),
+          map((products) => ({
+            category: productGroup.key,
+            products: products,
+          }))
+        )
+      ),
+      toArray()
+    );
   }
 }
